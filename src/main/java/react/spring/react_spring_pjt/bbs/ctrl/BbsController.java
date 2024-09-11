3c66959dc12d0fbafc90870cc4cff5bae1e66cbd
Yuminyumin <102581107+Yuminyumin@users.noreply.github.com>
2024-09-11 17:40:39 +0900

    Update BbsController.java

--- src/main/java/react/spring/react_spring_pjt/bbs/ctrl/BbsController.java
+++ src/main/java/react/spring/react_spring_pjt/bbs/ctrl/BbsController.java
@@ -20,7 +20,6 @@ import java.util.Map;
 import java.util.HashMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
 
 
